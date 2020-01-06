b976ab6efc79088f06c5c9496be64e3c96781fec
Robert Solli <solli.robert@gmail.com>
2020-01-06 16:22:31 +0100

    changed behaviour to symlink against key in repo

--- enable_ssh.sh
+++ enable_ssh.sh
@@ -1,4 +1,2 @@
 #! /bin/bash
-key=$( cat /xal/internal/pub_keys/$1/*)
-touch /home/$1/.ssh/authorized_keys
-echo $key >> /home/$1/.ssh/authorized_keys
+ln -s /xal/internal/pub_keys/$1/* /home/$1/.ssh/authorized_keys
