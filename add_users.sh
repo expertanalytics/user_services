0e9f94eb90ab9dbdd13615a46d94224016a4f13c
copperwire <solli.robert@gmail.com>
2020-01-06 13:30:27 +0000

    added encrypt and modified creat user

--- add_users.sh
+++ add_users.sh
@@ -2,16 +2,23 @@
 
 #only execute if root
 if [ $(id -u) -eq 0 ]; then
+	cd /root/internal
+	git pull
 	#iterate over added ssh keys
 	for dir in /root/internal/pub_keys/*/
 	do
 		dir=${dir%*/}
 		username=${dir##*/}
 		egrep "$username" /etc/passwd >/dev/null
-		if [ $? -eq 0 ]; then
-			echo $username exists!
-		else
-			echo $username doesnt exist!
+		if [ ! $? -eq 0 ]; then
+			echo adding user $username
+			useradd -m -s /bin/bash $username
+			chown -R $username /home/$username
+			if [ -f /etc/nebula/ca.key ]; then
+				bash /root/user_services/encrypt_cert.sh $username
+			fi
 		fi
 	done
+else
+	echo script must be run as root, exting
 fi
